e6d2e7d6bbe4a55bf97b3b6456cb1f18b4b47f4c
Carmina <carminalong@gmail.com>
2019-05-24 20:10:29 -0400

    add song number

--- src/components/Album.js
+++ src/components/Album.js
@@ -37,6 +37,7 @@ class Album extends Component {
           <tbody>
             {this.state.album.songs.map((song, index) =>
                 <tr className="song" key={index} >
+                  <td className="song-number">{index + 1}</td>
                   <td className="song-title">{song.title}</td>
                   <td className="song-duration">{song.duration}</td>
                 </tr>
